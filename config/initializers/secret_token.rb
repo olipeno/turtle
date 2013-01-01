5d9a90d55fcbf1535d1291a0b07b3965e71f75e7
prachi sakharkar <olipeno@gmail.com>
2012-12-31 17:30:40 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turtle::Application.config.secret_token = 'fb5f437e1aa03a671297e3fde4a59431227b19a3899b33788bb6bf4e62d6c23d110b5758e4b76fbc9b6c849578602ff33a881d80eeb812c3bfc42b3e07b35b30'
